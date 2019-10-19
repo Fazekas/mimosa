c690c7385f7eef1c3fa5c24f85c5d2e746fbcb11
NFazekas <nfazekas@nexient.com>
2019-10-19 17:09:59 -0400

    Added login page

--- component/LoginComponent.js
+++ component/LoginComponent.js
@@ -1,15 +1,81 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput } from 'react-native';
-import {Input} from 'react-native-elements';
-export default class LoginComponent extends Component {
-    render() {
-        return (
-          <View>
-              <Text>Hello world</Text>
-              <TextInput></TextInput>
-              <form><input type="text"/></form>
+import { View } from 'react-native';
+import { Input, Button, Text } from 'react-native-elements';
 
-          </View>
-        );
+export default class LoginComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: '', password: '', isLogIn: false};
+  }
+
+  setEmail(email) {
+    console.log('email');
+    console.log(email);
+    this.setState({email});
+    console.log('state');
+  }
+
+  setPassword(password) {
+    this.setState({password})
+  }
+
+  setIsLogin() {
+    this.setState({isLogIn: true});
+  }
+
+  isSubmitDisabled() {
+    console.log('submitdisabled', this.state);
+    if (this.state.password !== undefined) {
+      return false
     }
+    return true;
+  }
+
+  showLogin() {
+    return (
+      <View>
+        <View>
+          <Input onChangeText={(email) => this.setEmail(email)} label='Email' placeholder='email@address.com' errorMessage='Please enter a valid email address'/>
+        </View>
+        <View>
+          <Input label='Password' onChangeText={(password) => this.setPassword(password)} placeholder='Password'/>
+        </View>
+        <View>
+          <Button title='Submit' onPress={() => this.tappedSubmit()} disabled={this.isSubmitDisabled()} type='solid'/>
+        </View>
+      </View>
+    );
+  }
+
+  showLoginButton() {
+    return (
+      <View>
+        <View>
+          <Button title='Login' onPress={() => this.setIsLogin()}/>
+        </View>
+        <View>
+          <Text>Create A New User</Text>
+        </View>
+      </View>
+    )
+  }
+
+  tappedSubmit() {
+    console.log('tapped button');
+    console.log(this.state);
+  }
+
+  render() {
+    let shouldLogin;
+    if (this.state.isLogIn) {
+      shouldLogin = this.showLogin();
+    } else {
+      shouldLogin = this.showLoginButton();
+    }
+    return (
+        <View>
+          {shouldLogin}
+        </View>
+      );
+  }
 }
